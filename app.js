769104842808401138c844198e6ed022cf215cfa
Darsh <darshjain2310@gmail.com>
2024-03-08 19:53:46 +0530

    8 march

--- app.js
+++ app.js
@@ -15,45 +15,66 @@ const app = express()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(express.static(__dirname+ '\\public'));
+app.use(express.static(__dirname + '\\public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var users = []
+var usercredit = []
+var count = 0;
+
 
 app.get('/', function (req, res) {
-        res.render('index.ejs',{
-                'name' : ' '
+        res.render('index.ejs', {
+                'name': ' '
         })
-      })
+})
 
 
-app.post('/',function(req,res){
+app.post('/', function (req, res) {
         var a = req.body.fname
-        var b = "8aae3a73a9a43ee6b04dfd986fe9d136"
- if((md5(a))==b){
-        res.render('index.ejs',{
-                'name':"login successfully"
-        })
-}
-else{
-        res.render('index.ejs',{
-                'name':"login unsuccessfully"
-        })
-}
+        var validuser = 0;
+
+        for (let i = 0; i < count; i++) {
+                if (a == users[i]['userid']) {
+                        validuser = 1;
+                }
+        }
+        if (validuser == 1) {
+                console.log("valid user")
+        }
+        else {
+                console.log("Invalid user")
+        }
+
 })
 
 app.get('/register', function (req, res) {
-        res.render('register.ejs',{
-                // 'name' : ' '
-        })
-      })
-
-app.post('/register',function(req,res){
-        users.push(req.body)
         res.render('register.ejs')
-        console.log(users)
 })
 
-app.listen(2000,function(req,res){
+count = 0;
+app.post('/register', function (req, res) {
+
+        count++
+
+        var userid = req.body.userid
+        var fname = req.body.fname
+        var pass = (md5(req.body.pass))
+
+        usercredit['userid'] = userid
+        usercredit['name'] = fname
+        usercredit['password'] = pass
+
+        users.push(structuredClone(usercredit))
+
+        for (let i = 0; i < count; i++) {
+                console.log(users[i]['userid'])
+        }
+
+
+        res.render('register.ejs')
+})
+
+app.listen(2000, function (req, res) {
         console.log("Server started")
 })
